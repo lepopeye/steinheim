09ac5739afe467460de5cd686596de2ad9f5d58d
Jat <jinan15@hotmail.com>
2012-08-30 20:01:31 +0200

    Environement refonte de Noyade
    J'ai remplacé les abm par minetest.register_globalstep(function(dtime)
    pour lance l'opération tous les 1 seconde et je d'un dictionnaire pour
    engistre pseudo et quand le personnage est rentrée dans l'eau. Si le
    personnage sort de l'eau la  clés dans le dictionnaire est supprimé. si
    il reste + 10 seconde dans l'eau il prend des dégat.

--- environement/init.lua
+++ environement/init.lua
@@ -1,54 +1,38 @@
---Environement by Jat
---Licence GPL
---Pour ce mod modifier les joueur il faut rajouté 3 ligne 
 --[[
-List environement 1
-Item air 10
-EndInventoryList
+Environement by Jat
+Licence GPLv2 or later
 --]]
 -- Noyade
-minetest.register_abm(	-- fonction permettant d'assigner une action à un bloc
-    {nodenames = {"default:water_flowing","default:water_source"},	-- nom du bloc
-    interval = 1.0,	-- ???
-    chance = 1,	-- ???
-    action = function(pos)	-- fonction associée au bloc
-		local objs = minetest.env:get_objects_inside_radius(pos, 1)	-- variable d'environnement : quand le joueur est dans le bloc
-		for k, obj in pairs(objs) do	-- boucle "for" :
-			if obj:is_player() then
-				local position = obj:getpos()
-				position.y = position.y+1.5
-				local nodename = minetest.env:get_node(position).name
-				if string.find(nodename, "default:water") then
-					if obj:get_inventory():get_stack("environement",1):get_count()>1 then
-						obj:get_inventory():remove_item('environement', 'air')
-					end
-					if obj:get_inventory():get_stack("environement",1):get_count()<=1 then
-						obj:set_hp(obj:get_hp()-0.5) -- give 1HP -- augmenter la vie HP du joueur de +1
-					end
-					minetest.chat_send_player(obj:get_player_name(), obj:get_inventory():get_stack("environement",1):get_count())
-				else
-					if obj:get_inventory():get_stack("environement",1):get_count()<10 then
-						obj:get_inventory():add_item('environement', 'air 1')
-					end
-				end
+local lestimer={}
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer+dtime
+	if timer < 1 then
+		return
+	end
+	timer = 0
+
+	for _,obj in ipairs(minetest.get_connected_players()) do
+		local position = obj:getpos()
+		position.y = position.y+1.5
+		local nodename = minetest.env:get_node(position).name
+		if string.find(nodename, "default:water") then
+			if lestimer[""..obj:get_player_name()..""]==nil then
+				lestimer[""..obj:get_player_name()..""]= os.clock()
+			end
+			if os.clock()>=lestimer[""..obj:get_player_name()..""]+10 then
+				obj:set_hp(obj:get_hp()-0.5)
+			end
+		else
+			if not(lestimer[""..obj:get_player_name()..""]==nil) then
+				lestimer[""..obj:get_player_name()..""] = nil
 			end
 		end
-    end,
-})
-minetest.register_abm(	-- fonction permettant d'assigner une action à un bloc
-    {nodenames = {"air"},	-- nom du bloc
-    interval = 1.0,	-- ???
-    chance = 1,	-- ???
-    action = function(pos, node, active_object_count, active_object_count_wider)	-- fonction associée au bloc
-		local objs = minetest.env:get_objects_inside_radius(pos, 1)	-- variable d'environnement : quand le joueur est dans le bloc
-		
-		for k, obj in pairs(objs) do	-- boucle "for" :
-			if obj:is_player() then
-				if obj:get_inventory():get_stack("environement",1):get_count()<10 then
-					obj:get_inventory():add_item('environement', 'air 1')
-				end
-				--minetest.chat_send_player(obj:get_player_name(), obj:get_inventory():get_stack("environement",1):get_count())
-			end
-		end
-    end,
-})
+	end
+end)
+
+
+
+
+
+	
