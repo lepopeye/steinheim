92b0d2e369b671d1dab8e39ef030c286b95e6bb5
Jat <jinan15@hotmail.com>
2012-11-07 17:34:48 +0100

    Mise a jour du mod mort et ajout des mods
    Modification de mort
    Ajout de bags et inventory_plus
    "BUG": Impossible de supprimés les bags

--- mort/init.lua
+++ mort/init.lua
@@ -1,15 +1,28 @@
 --[[
-Mort V0.2 par Jat
+Mort V0.3 par Jat
 Licence GPLv2 or later for code
 
 Nettoye tout l'inventaire
 Nettoye la partie craft
 Ajout d'un message dans le ALL quand quelqu'un meurt.
+Efface les ce qui y a dans les sacs, sans effacé ces dernier.
 --]]
 minetest.register_on_dieplayer(function(player)
---	player:get_inventory():set_size('main', 0)
---	player:get_inventory():set_size('main', 32)
---	player:get_inventory():set_size('craft', 0)
---	player:get_inventory():set_size('craft', 9)
+	local function mort(list)
+		local taille=player:get_inventory():get_size(list)	
+		player:get_inventory():set_size(list, 0)
+		player:get_inventory():set_size(list, taille)
+	end
+	mort("craft")
+	mort("main")
+	for i=1,4 do
+		if not(player:get_inventory():is_empty('bag'..i..'')) then
+			mort("bag"..i.."contents")
+--			Impossible de supprimé les sacs a causse d'un bug de bags ou inventory plus dans le Gui (rafraichisement)
+--			player:get_inventory():set_size('bag1', 0)
+--			player:get_inventory():set_size('bag1', 1)
+--			player:get_inventory():remove_item('bag1', "bags:small")
+		end
+	end
 	minetest.chat_send_all(''..player:get_player_name()..' est mort.')
 end)
