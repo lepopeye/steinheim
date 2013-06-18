7fcbe5f4e0ad287300fface743528c250a4e7f69
Jat <jinan15@hotmail.com>
2013-06-18 17:10:16 +0200

    Mod Clean fix (pour craft_guide)

--- clean/init.lua
+++ clean/init.lua
@@ -1,4 +1,4 @@
-local old_nodes = {"hydro:wild_tomato","hydro:wild_peas","hydro:wild_habanero","hydro:wild_grapes","hydro:wild_coffee","hydro:wild_roses","flowers:flower_cotton","farming_plus:rubber_sapling","farming_plus:rubber_tree_full","farming_plus:rubber_leaves","farming_plus:rubber_tree_empty"}
+local old_nodes = {"hydro:wild_tomato","hydro:wild_peas","hydro:wild_habanero","hydro:wild_grapes","hydro:wild_coffee","hydro:wild_roses","flowers:flower_cotton","farming_plus:rubber_sapling","farming_plus:rubber_tree_full","farming_plus:rubber_leaves","farming_plus:rubber_tree_empty","craft_guide:sign_wall","craft_guide:lcd_pc"}
 local old_entities = {}
 
 for _,node_name in ipairs(old_nodes) do
