8c17b1a1082d214aa12650d3858b644695db5c63
Jat <jinan15@hotmail.com>
2013-07-23 02:48:19 +0200

    Mod clean modifier
    -Supression des reste de ufos
    -Liste plus facilement lissible

--- clean/init.lua
+++ clean/init.lua
@@ -1,5 +1,9 @@
-local old_nodes = {"hydro:wild_tomato","hydro:wild_peas","hydro:wild_habanero","hydro:wild_grapes","hydro:wild_coffee","hydro:wild_roses","flowers:flower_cotton","farming_plus:rubber_sapling","farming_plus:rubber_tree_full","farming_plus:rubber_leaves","farming_plus:rubber_tree_empty","craft_guide:sign_wall","craft_guide:lcd_pc"}
-local old_entities = {}
+local old_nodes = {"hydro:wild_tomato","hydro:wild_peas","hydro:wild_habanero","hydro:wild_grapes","hydro:wild_coffee","hydro:wild_roses",
+					"flowers:flower_cotton",
+					"farming_plus:rubber_sapling","farming_plus:rubber_tree_full","farming_plus:rubber_leaves","farming_plus:rubber_tree_empty",
+					"craft_guide:sign_wall","craft_guide:lcd_pc",
+					"ufos:box"}
+local old_entities = {"ufos:ufo"}
 
 for _,node_name in ipairs(old_nodes) do
 	minetest.register_node(":"..node_name, {
