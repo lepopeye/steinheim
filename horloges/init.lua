cd9354db0ea7f5dde3e1abd2f40958a05e0cd149
Jat <jinan15@hotmail.com>
2013-11-29 13:59:01 +0100

    Mod horloges ajouté
    - Ajoute l'heur en bas a gauche

---
+++ horloges/init.lua
@@ -0,0 +1,33 @@
+horlogeshud = {}
+
+minetest.register_globalstep(function(dtime)
+        for _, player in pairs(minetest.get_connected_players()) do
+                local name = player:get_player_name()
+                if not horlogeshud[name] then
+                        horlogeshud[name] = {}
+                        horlogeshud[name].id = player:hud_add({
+                                hud_elem_type = "text",
+                                name = "Horloges",
+                                number = 0xFFFFFF,
+                                position = {x=0, y=1},
+                                offset = {x=5, y=-60},
+                                direction = 0,
+                                text = ""..os.date("%H")..":"..os.date("%M").."",
+                                scale = {x=50, y=20},
+                                alignment = {x=1, y=1},
+                        })
+                        horlogeshud[name].minute = os.date("%M")
+                        return
+                end
+                if horlogeshud[name].minute ~= os.date("%M") then
+                        player:hud_change(horlogeshud[name].id, "text",""..os.date("%H")..":"..os.date("%M").."")
+                        horlogeshud[name].minute = os.date("%M")
+                end
+        end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+        horlogeshud[player:get_player_name()] = nil
+end)
+
+	
