364abb8159271edd4c7270e5bd2d43ab253f8494
Jat <jinan15@hotmail.com>
2013-12-01 07:31:41 +0100

    Mod horloges mis a jour

--- horloges/init.lua
+++ horloges/init.lua
@@ -1,6 +1,13 @@
 horlogeshud = {}
 
+local timer = 0
+
 minetest.register_globalstep(function(dtime)
+	timer = timer+dtime
+	if timer < 60 then
+		return
+	end
+	timer=0
         for _, player in pairs(minetest.get_connected_players()) do
                 local name = player:get_player_name()
                 if not horlogeshud[name] then
